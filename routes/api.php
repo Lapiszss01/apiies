f04f8dab3ec683d65e77c76071d54347bc80407e
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-10-29 16:42:08 +0100

    Fin paso 8

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,11 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('lists/categories', [CategoryController::class, 'list']);
+
 Route::get('categories',[CategoryController::class,'index']);
 Route::get('categories/{category}',[CategoryController::class,'show']);
+Route::post('categories',[CategoryController::class,'store']);
 
 Route::get('products',[ProductController::class,'index']);
+
 
